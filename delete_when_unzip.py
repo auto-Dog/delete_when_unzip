39fd087803af51b6a63e3c67ecef0162fbf87f2e
auto-Dog <62736484+auto-Dog@users.noreply.github.com>
2023-09-15 00:32:30 +0800

    Add files via upload

---
+++ delete_when_unzip.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# TODO: 设法完成分段zip的解压
+import os
+from stream_unzip import stream_unzip
+
+def shift_then_truncate(file,chunk_size=1024):
+    '''将文件向头部平移chunksize，并保留未被覆盖的后半部分'''
+    with open(file,'rb+') as f: 
+        pointer = chunk_size
+        while True:
+            f.seek(pointer)
+            chunk = f.read(chunk_size)
+            if not chunk:
+                break
+            new_pointer = f.tell()
+            f.seek(pointer-chunk_size)
+            f.write(chunk)  # 将第k段写入k-1段的空间内
+            pointer = new_pointer   # 指向第k段末尾
+            # print('shift once') # debug
+        f.seek(pointer-chunk_size)  # 指向平移后的末尾
+        f.truncate()
+
+def read_file_by_chunk(file,chunk_size=1024):
+    '''按块读取文件，可指定块大小'''
+    while True:
+        with open(file,'rb') as f:  # 存疑，压缩文件内容是什么编码？
+            f.seek(0)
+            chunk = f.read(chunk_size)
+            pointer = f.tell()
+            if not chunk:
+                return
+        shift_then_truncate(file,chunk_size)# [chunk_size:-1]的文件内容逐次向左移动，最后截断只保留这部分内容
+        yield chunk
+# def remove_one_chunk(file,chunk_size=1024):
+#     '''删除文件头部若干长度的内容'''
+#     f_size = os.path.getsize(file)
+#     # print(f_size)
+#     with open(file,'rb+') as f:
+#         if f_size<chunk_size:
+#             chunk_size = f_size
+#         f.seek(chunk_size)
+#         f.truncate(f_size-chunk_size)
+#         f.seek(0)
+#     print('Removed {:.2f} MB'.format(chunk_size/1024/1024))
+
+def main_unzip(file,chunk_size=1024):
+    '''在本地流式解压文件，边解压边删除'''
+    file_chunks = read_file_by_chunk(file,chunk_size)
+    i = 0
+    for file_path_name, file_size, unzipped_chunks in stream_unzip(file_chunks,chunk_size=chunk_size):
+        # print('Processing chunk {}'.format(i))  # debug
+        i+=1
+        file_path_name = os.path.join('./test_folder/',file_path_name.decode('GBK'))
+        dir_path,file_name = os.path.split(file_path_name)  # 检查文件存放路径的健全性
+        # print(dir_path,file_name) # debug
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        if file_name != "":
+            with open(file_path_name,'wb+') as f:
+                for chunk in unzipped_chunks:
+                    f.write(chunk)
+                f.close()
+
+if __name__ == '__main__':
+    multi_zip_seg = True
+    CHUNK_SIZE = 1024*1024*512  # 512MB per chunk
+    if multi_zip_seg:
+        file_path_list = ['./testm.zip','./testm.z01']
+        for zip_files in file_path_list:
+            main_unzip(zip_files,CHUNK_SIZE)
+    else:
+        FILE_PATH = r'./test.zip'
+        
+        main_unzip(FILE_PATH,CHUNK_SIZE)
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Delete When Unzip
+# 边解压边删除的解压工具
+---
+_对于大型游戏压缩文件解压需要翻倍空间的情况，这是一个值得拥有的工具_  
+_GOODNEWS FOR GAMERS! Big ZIP files can be unzipped under limited disk space，no need for doubled space_ 
+
+* 通常，100G的游戏压缩文件在解压过程中需要至少200G的空间，对不愿意硬盘大量扩容的朋友及其不友好
+* Usually, a 100G game requires at least 200G disk space when unzip, which is bad for users with limited space
+<br></br>
+  
+* 本工具利用流式解压`stream_unzip`，将本地文件按顺序流式读取到内存，每次读取固定大小的块并解压。在解压过一部分块后，立即删除原压缩文件的块，因此解压过程中对硬盘的占用将远远小于压缩文件的2倍。
+
+* The program imports `stream_unzip` and sees local ZIP file as a stream. For each iteration it reads a chunk into memory then apply unzip algorithm. After 'unzip a chunk', it deteles the in original ZIP file, thus keeps a low usage of disk space(at least you donnot need doubled space to unzip)
+
+-[] TODO: 目前只能解压单个文件，需要研究分卷zip文件的解压。 Need implementation for segmented zip
